37f252f6ba5e9bfd4aa6a864a526c3d02662e15d
Raju1-Ahmed <robiussaniraju@gmail.com>
2023-07-27 03:23:17 +0600

    almost done without contract and skills

--- app.js
+++ app.js
@@ -6,13 +6,44 @@ const fileUpload = require('./routes/fileupload.route');
 const fileDownload = require('./routes/fileupload.route');
 const products = require('./routes/fileupload.route');
 
+const dotenv = require('dotenv');
+const mg = require('mailgun-js');
 
+dotenv.config();
+
+const mailgun = () =>
+  mg({
+    apiKey: process.env.MAILGUN_API_KEY,
+    domain: process.env.MAILGUN_DOMAIN,
+  });
 
 //middleware
 app.use(express.json());
 app.use(cors());
+app.use(express.urlencoded({ extended: true }));
 
-
+app.post('/api/email', (req, res) => {
+  const { email, subject, message } = req.body;
+  mailgun()
+    .messages()
+    .send(
+      {
+        from: `robiussaniraju@gmail.com` ,
+        to: `${email}`,
+        subject: `${subject}`,
+        html: `<p>${message}</p>`,
+      },
+      (error, body) => {
+        if (error) {
+          console.log(error);
+          res.status(500).send({ message: 'Error in sending email' });
+        } else {
+          console.log(body);
+          res.send({ message: 'Email sent successfully' });
+        }
+      }
+    );
+});
 
 app.get("/", (req, res) => {
   res.send("Route is working! YaY!");
